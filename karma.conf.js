90bc62ed2075701b43a86ecd3b5201298f6e150c
Jessica Massango <54636889+Jessie116@users.noreply.github.com>
2022-09-07 21:30:42 +0200

    fixed index

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/exduties-app'),
+      dir: require('path').join(__dirname, './coverage/exchange-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
